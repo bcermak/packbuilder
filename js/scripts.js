d8acae286c761b33df32060515545017aabb6045
Brandon Cermak <cermakprofessional@gmail.com>
2022-02-03 17:41:07 -0600

    got the list item to apppend into listDiv

--- js/scripts.js
+++ js/scripts.js
@@ -8,26 +8,31 @@ const item4 = document.getElementById("item4");
 const array = [item1, item2 ]
 
 item1.addEventListener("click", myFunction1);
-item2.addEventListener("click", myFunction2)
+item2.addEventListener("click", myFunction2);
 item3.addEventListener("click", myFunction3);
-item4.addEventListener("click", myFunction4)
+item4.addEventListener("click", myFunction4);
 
 function myFunction1() {
+    let listDiv = document.getElementById("listDiv");
     this.style.backgroundColor = "lightgreen";
-    console.log(this);
+    listDiv.append(this);
+
 }
 
 function myFunction2(){
+    let listDiv = document.getElementById("listDiv");
     this.style.backgroundColor = "purple";
-    console.log(this);
+    listDiv.append(this);
 }
 
 function myFunction3(){
+    let listDiv = document.getElementById("listDiv");
     this.style.backgroundColor = "pink";
-    console.log(this);
+    listDiv.append(this);
 }
 
 function myFunction4(){
+    let listDiv = document.getElementById("listDiv");
     this.style.backgroundColor = "yellow";
-    console.log(this);
+    listDiv.append(this);
 }
