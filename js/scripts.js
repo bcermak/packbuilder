a36ba9ae1086f947b02fb7cf342fc484cff2b851
Brandon Cermak <cermakprofessional@gmail.com>
2022-02-04 12:17:12 -0600

    added icon to h2 and a Google Font

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 // Event Listener for list item path - appending path to listDiv after click.
 
-let button = document.getElementsByClassName("item");
+const button = document.getElementsByClassName("item");
+
 
 if (button) {
     for (let i =0; i < button.length; i++) {
         button[i].addEventListener("click", btnItem => {
-            // console.log(btnItem.path[0]);
+            console.log(btnItem.path[0]);
             let listDiv = document.getElementById("listDiv");
-            listDiv.append(btnItem.path[0]);
-            
+            listDiv.append(btnItem.path[0]);  
         })
     }
 }
